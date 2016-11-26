17f21550bcd20b060cd0f698a745bf9d1278241f
Vinicius Cavalcanti <vincius.cavalcanti16@gmail.com>
2016-11-26 17:19:10 -0200

    adicionado eventos blur e focus nos inputs dos contatos

--- assets/resources/js/contato/contato.js
+++ assets/resources/js/contato/contato.js
@@ -1,25 +1,86 @@
 var $$ = function (id) {
-    return document.getElementById(id);
+  return document.getElementById(id);
 }
 
 function validaContato() {
-    var nome == $$("name").value;
-    var email == $$("email").value;
-    var message == $$("message").value;
+  var nome = $$("name").value;
+  var email = $$("email").value;
+  var message = $$("message").value;
+
+  nome.addEventListener('blur', function() {
     if ((nome === null) || (nome === "")) {
-        document.alert("Preencha o campo nome");
-        return false;
-    } else if ((email === null) || (email === "")) {
-        document.alert("Preencha o campo email");
-        return false;
-    } else if ((message === null) || (message === "")) {
-        document.alert("Preencha o campo mensagem");
-        return false;
+      document.alert("Preencha o campo nome");
+      return false;
     }
-    enviadoContato();
-    return true;
+  });
+    
+  if ((email === null) || (email === "")) {
+    document.alert("Preencha o campo email");
+    return false;
+  } else if ((message === null) || (message === "")) {
+    document.alert("Preencha o campo mensagem");
+    return false;
+  }
+  enviadoContato();
+  return true;
 }
 
 function enviadoContato() {
-    document.getElementById("contact-form").innerHTML = ("REGISTROS ENVIADOS");
+  document.getElementById("contact-form").innerHTML = ("REGISTROS ENVIADOS");
+}
+
+window.onload = function() {
+  var input_name = document.forms[0].name;
+  var input_email = document.forms[0].email;
+  var input_msg = document.forms[0].message;
+  
+  input_name.addEventListener('blur', function() {
+    if (input_name.value == "")
+      input_name.value = "Você esqueceu de preencher-me";
+  });
+  
+  input_email.addEventListener('blur', function() {
+    if (input_email.value == "")
+      input_email.value = "Você esqueceu de preencher-me";
+  });
+  
+   input_msg.addEventListener('blur', function() {
+    if (input_msg.value == "")
+      input_msg.value = "Você esqueceu de preencher-me";
+  });
+  
+  input_name.addEventListener('focus', function() {
+    input_name.style.color = '#fff';
+    input_name.style.background = '#0080ff';
+    
+    input_email.style.color = '#000';
+    input_email.style.background = '#fff';
+    
+    input_msg.style.color = '#000';
+    input_msg.style.background = '#fff';
+  });
+  
+  input_email.addEventListener('focus', function() {
+    input_name.style.color = '#000';
+    input_name.style.background = '#fff';
+    
+    input_email.style.color = '#fff';
+    input_email.style.background = '#0080ff';
+    
+    input_msg.style.color = '#000';
+    input_msg.style.background = '#fff';
+  });
+  
+  input_msg.addEventListener('focus', function() {
+    input_msg.style.color = '#fff';
+    input_msg.style.background = '#0080ff';
+    
+    input_email.style.color = '#000';
+    input_email.style.background = '#fff';
+    
+    input_name.style.color = '#000';
+    input_name.style.background = '#fff';
+  });
+  
+  
 }
